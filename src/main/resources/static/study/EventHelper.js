d6739439b3cc3ae91dd8b557233b1658fdfb0176
AD-Wu <944907103@qq.com>
2020-06-23 09:04:20 +0800

    -

--- src/main/resources/static/study/EventHelper.js
+++ src/main/resources/static/study/EventHelper.js
@@ -23,6 +23,41 @@ let EventHelper = {
     getTarget: function (event) {
         return event.target || event.srcElement;
     },
+    getButton: function (event) {
+        if (document.implementation.hasFeature("MouseEvents", "2.0")) {
+            return event.button;
+        } else {
+            switch (event.button) {
+                // 没有按下按钮
+                case 0:
+                // 按下了主鼠标按钮
+                case 1:
+                // 同时按下了主、次鼠标按钮
+                case 3:
+                // 同时按下了主、中间鼠标按钮
+                case 5:
+                // 同时按下了三个鼠标按钮
+                case 7:
+                    return 0;// 主
+                // 按下了次鼠标按钮
+                case 2:
+                // 同时按下了次、中间鼠标按钮
+                case 6:
+                    return 2// 次
+                // 按下了中间鼠标按钮
+                case 4:
+                    return 1;// 中间
+            }
+        }
+    },
+    getWheelDelta: function (event) {
+        if (event.wheelDelta) {
+            return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+        } else {
+            // Firefox
+            return -event.detail * 40;
+        }
+    },
     preventDefault: function (event) {
         if (event.preventDefault) {
             event.preventDefault();
